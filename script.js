84e2de84c00a2b11aed9ec35584f85bf5f74788e
gim8912 <gim8912@gmail.com>
2014-09-12 14:45:15 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, create: create});
+var cow;
+
+function preload() {
+	game.load.spritesheet('cow', 'assets/cow.png',100 , 50);
+}
+
+function create() {
+	cow = game.add.sprite(100, 100, 'cow');
+	cow.animations.add('walk');
+	cow.inputEnabled = true;
+	cow.events.onInputDown.add(cowClick, this);
+}
+
+function cowClick() {
+	cow.animations.play('walk', 5, false);
+}
