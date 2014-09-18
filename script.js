47562eeb9a61bcf9eaedd1577ab1c88866067365
gim8912 <gim8912@gmail.com>
2014-09-18 18:41:20 +0900

    update 09.18

--- script.js
+++ script.js
@@ -3,19 +3,92 @@ var game = new Phaser.Game(3840, 2160, Phaser.AUTO, '',
 var bg;
 var cow;
 
+//// 강승화
+var tractor;
+var bird;
+var lift;
+var tree;
+var train;
+var house01;
+
 function preload() {
 	game.load.spritesheet('cow', 'assets/cow.png',100 , 50);
 	game.load.image('bg', 'assets/bg.jpg');
+	
+	//강승화
+	game.load.spritesheet('tractor', 'assets/tractor.png', 82/*width*/,110/*height*/);
+	game.load.spritesheet('bird', 'assets/bird01.png', 86/*width*/,109/*height*/);
+	game.load.spritesheet('lift', 'assets/lift.png', 56/*width*/,40/*height*/);
+	game.load.spritesheet('tree', 'assets/tree01.png', 184/*width*/,159/*height*/);
+	game.load.spritesheet('train', 'assets/train.png', 551/*width*/,72/*height*/);
+	game.load.image('house01', 'assets/house01.png');
+	game.load.image('house02', 'assets/house02.png');
+	//
 }
 
-function create() {
-	bg = game.add.sprite(0,0, 'bg')
-	cow = game.add.sprite(100, 100, 'cow');
-	cow.animations.add('walk');
-	cow.inputEnabled = true;
-	cow.events.onInputDown.add(cowClick, this);
-}
+tractor = game.add.sprite(1000, 1050, 'tractor'); //트렉터
+	tractor.animations.add('walk');
+	tractor.inputEnabled = true;
+	tractor.events.onInputDown.add(tractorClick, this);
 
+	bird = game.add.sprite(790,1330, 'bird');
+	bird.animations.add('fly');
+	bird.animations.play('fly', 10/*속도조절*/, true);
+	//bird.inputEnabled = true;
+	//bird.events.onInputDown.add(birdClick, this);
+
+	lift = game.add.sprite(2400, 180, 'lift'); //리프트
+	lift.animations.add('swing');
+	lift.inputEnabled = true;
+	lift.events.onInputDown.add(liftClick, this);
+
+	tree = game.add.sprite(300, 1050, 'tree'); //나무
+	tree.animations.add('cut');
+	tree.inputEnabled = true;
+	tree.events.onInputDown.add(treeClick, this);
+
+	train = game.add.sprite(3000, 440, 'train'); //기차
+	train.animations.add('shiny');
+	train.inputEnabled = true;
+	train.events.onInputDown.add(trainClick, this);
+
+}
 function cowClick() {
-	cow.animations.play('walk', 5, false);
+	cow.animations.play('walk', 10, false);	
+}
+
+//강승화
+function tractorClick() {
+	tractor.animations.play('walk', 7, false);
+}
+/*
+function tractorClick(){
+	tractor.animations.play('walk', 7, false);
+	tractor.x-=100;
+
+	if (tractor.x < -tractor.width) 
+	{
+		//tractor.x =game.bg.width;
+		tractor.x =game.bg.width;
+	};
+}
+function update() {
+	tractor.x-=2;
+
+	if (tractor.x < -tractor.width) 
+	{
+		//tractor.x =game.bg.width;
+		tractor.x =game.bg.width;
+	};
+
+}
+*/
+function liftClick() {
+	lift.animations.play('swing', 8, false);
+}
+function treeClick() {
+	tree.animations.play('cut', 16, false);
+}
+function trainClick() {
+	train.animations.play('shiny', 8, false);
 }
